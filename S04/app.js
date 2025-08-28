69a44de65c5bc02645ad910ba64aef73dc2bf8c6
olinca <olinca.laguna@gmail.com>
2025-08-28 14:39:59 -0600

    S04

--- S04/app.js
+++ S04/app.js
@@ -1,1 +1,33 @@
 console.log("Hola, mundo!");
+console.log(THREE);
+
+//Definir nuestro canvas
+const canvas = document.getElementById("lienzo");
+
+//Definir variables del tamaño de la ventana 
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+//Actualizamos la resolución del canvas
+canvas.width = width;
+canvas.height = height;
+
+//Código para configurar una escena 
+const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+const mesh = new THREE.Mesh(
+   new THREE.SphereGeometry(),
+   new THREE.MeshBasicMaterial({ color: "#ff6600", wireframe: true })
+);
+
+//Agregar nuestro objeto a la escena 
+scene.add(mesh);
+
+
+//mover nuestro mesh en la escena
+mesh.position.z = -5;
+
+
+//para renerizar lo que esta viendo nuestra escena
+renderer.render(scene, camera);
